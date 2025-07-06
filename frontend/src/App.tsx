449995c71264c5068315822fb71a914a7fb0b384
Swarnavo2003 <swarnavo2020@gmail.com>
2025-07-06 22:16:45 +0530

    01-First Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-10">App</h1>
+      <Button>HERO NANDAN</Button>
+    </div>
+  );
+};
+
+export default App;
